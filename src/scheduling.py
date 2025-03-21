fc8b4b998c6c222dc3b0fbc1acd61b73a94dfe3c
TheAp0cryphal <tanishk.sh@proton.me>
2025-03-21 14:03:21 -0700

    Minor Fixes

--- src/scheduling.py
+++ src/scheduling.py
@@ -6,14 +6,10 @@
 #workload is arbitrary? based on what? We can add later...
 
 def score_employee(employee, required_skills, shift_day):
+    
     # Convert skills into a set for comparison
-   
     emp_skills = set(employee['skills'].split(','))
     req_skills = set(required_skills.split(','))
-
-    if employee['id'] == 8:
-        print(emp_skills)
-        print(req_skills)
 
     # Calculate skill match score
     skill_score = len(emp_skills.intersection(req_skills))
@@ -26,8 +22,6 @@ def score_employee(employee, required_skills, shift_day):
     points_score = employee['points'] / 100.0  # normalize score ranged: [0 - 1]
 
     # Weighted sum (weights can be adjusted)
-    if employee['id'] == 8:
-        print(availability_score, skill_score, workload_score, points_score)
     total_score = availability_score * ((2 * skill_score) + (1 * workload_score) + (1 * points_score))
     return total_score
 
@@ -38,4 +32,4 @@ def match_employee(employees, shift):
         scores.append((employee['id'], score))
     # Sort by score descending
     scores.sort(key=lambda x: x[1], reverse=True)
-    return scores[0]  # Return the best candidate (employee id and score), heap can be used to boost fetch performance
+    return scores[0]  # Return the best candidate (employee id and score), heap can be used to boost fetch performance 
